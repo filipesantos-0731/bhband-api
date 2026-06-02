8833adc3e6c2632056984cafc9d8c35ba4260e62
Filipe <filipesantos.0731@gmail.com>
2026-06-02 17:34:04 -0300

    feat: adicionar interface completa com HTML + nova aba APIs

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.static(join(__dirname, '..', 'public')));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
